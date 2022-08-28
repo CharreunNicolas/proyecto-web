155e48a8e0a956eaf04326cf5d4564ed305603c6
nicocharreun <fncharreun96@gmail.com>
2022-08-27 23:03:12 -0300

    detalles-0

--- js/productos.js
+++ js/productos.js
@@ -1,16 +1,18 @@
 let contador = 0;
+
 const cantProductos = () =>{
     const seccion = document.getElementById('section-productos');
     contador++;
     const template = `
-    
+
           <img class="imagen img-fluid " src="../img/Img-Avion-producto.jpeg">
           <h5 class="card-title tituloDelProducto">Sonajero</h5>
           <h6 class="card-subtitle descripcion">Material: Hilo crochet</h6>
+          <h6 class="card-subtitle descripcion">Codigo: ${contador}</h6>
           <p class="card-text precio">$450</p> 
 
           <div class="div-boton-verProducto">
-          <button class="boton-verProducto">Ver Producto</button>
+          <button id="verProd-1" class="boton-verProducto">Ver Producto</button>
           </div>
           
           <a href="https://web.whatsapp.com/"  target="_blank" class="card-link card-link-wsp"><ion-icon id="wsp" name="logo-whatsapp"></ion-icon></a>
@@ -21,10 +23,11 @@ const cantProductos = () =>{
           <img class="imagen img-fluid" src="../img/Img-Avion-producto1.jpeg">
           <h5 class="card-title tituloDelProducto">Avion</h5>
           <h6 class="card-subtitle descripcion">Material: Lana</h6>
+          <h6 class="card-subtitle descripcion">Codigo: ${contador}</h6>
           <p class="card-text precio">$600</p> 
 
           <div class="div-boton-verProducto">
-          <button class="boton-verProducto">Ver Producto</button>
+          <button id="verProd-2" class="boton-verProducto">Ver Producto</button>
           </div>
 
           <a href="https://web.whatsapp.com/"  target="_blank" class="card-link card-link-wsp"><ion-icon id="wsp" name="logo-whatsapp"></ion-icon></a>
@@ -36,8 +39,10 @@ const cantProductos = () =>{
     divProducto.className = "card-body col-xs-6 col-sm-4 col-md-3 col-lg-2  cont-producto";
     //col-xs-6 col-sm-4 col-md-3 col-lg-2
     if(contador%2 == 0){
+     
       divProducto.innerHTML = template;
     }else{
+     
       divProducto.innerHTML = template2;
     }
     seccion.append(divProducto);
@@ -45,6 +50,9 @@ const cantProductos = () =>{
 }
 
 //Iterador de productos - (Dentro de la función no me deja iterar con un for)
+
 for(let i=0; i<23; i++){
   cantProductos();
 }
+
+
